032b73df119e46d5c800d81e12e708abb210bca4
SCHWMAX <max.schwenzer@voith.com>
2024-06-04 10:22:42 +0200

    offline swagger UI in fastapi with fastapi-offline package

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+# from fastapi import FastAPI
+from fastapi_offline import FastAPIOffline as FastAPI
 from fastapi.responses import FileResponse, Response
 
 import uvicorn
